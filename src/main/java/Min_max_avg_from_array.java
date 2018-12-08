f2fa7b1126ecdce1fc449ab4c0b5b87144b58aac
mihaiost <45467031+mihaiost@users.noreply.github.com>
2018-12-09 00:48:01 +0200

    Update Min_max_avg_from_array.java

--- src/main/java/Min_max_avg_from_array.java
+++ src/main/java/Min_max_avg_from_array.java
@@ -1,3 +1,11 @@
+/*Write a java program that will return the min, max and average values in a given array.
+
+For example 
+GIven the following array [13,20,14,5,2,8] the output should be
+Max : 20
+Min : 2
+Average: 10.3*/
+
 package main.java;
 
 public class Min_max_avg_from_array {
