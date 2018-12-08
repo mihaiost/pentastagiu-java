8995b415d9decf479b31f0c9b63b81e9a72aa2f2
mihaiost <45467031+mihaiost@users.noreply.github.com>
2018-12-09 00:42:16 +0200

    Update Count_odd_even_in_array.java

--- src/main/java/Count_odd_even_in_array.java
+++ src/main/java/Count_odd_even_in_array.java
@@ -1,3 +1,11 @@
+/*Write a Java program to find the number of even and odd integers in a given array of integers
+
+For example.
+Given the following array [13,20,14,5,2,8] the output should be:
+
+Odd elements count : 2
+Even elements count : 4*/
+
 package main.java;
 
 public class Count_odd_even_in_array {
