09b3b374a2e20bb90c46b98508b2cf1842c03d82
mihaiost <45467031+mihaiost@users.noreply.github.com>
2018-12-09 00:53:35 +0200

    Update Sum_up_all_nrs_to_target.java

--- src/main/java/Sum_up_all_nrs_to_target.java
+++ src/main/java/Sum_up_all_nrs_to_target.java
@@ -1,3 +1,6 @@
+/*Write a program that will sum up all numbers from 0 up to a target number.
+For example, for an input number 5, result should be 15 (0+1+2+3+4+5)*/
+
 package main.java;
 import java.util.Scanner;
 public class Sum_up_all_nrs_to_target {
