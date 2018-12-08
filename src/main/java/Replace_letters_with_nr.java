ec0c8a05496a69ac9acaa036e5b0b14bdfd26139
mihaiost <45467031+mihaiost@users.noreply.github.com>
2018-12-09 00:50:22 +0200

    Update Replace_letters_with_nr.java

--- src/main/java/Replace_letters_with_nr.java
+++ src/main/java/Replace_letters_with_nr.java
@@ -1,3 +1,15 @@
+/*Write a program that will return a different string representing the alphabet index of each letter.
+
+Assuming we have the following alphabet encoding
+
+a = 0
+b = 1
+c = 2
+d = 3
+…
+
+For the given string “abcd” the output should be “0123”*/
+
 package main.java;
 
 public class Replace_letters_with_nr {
