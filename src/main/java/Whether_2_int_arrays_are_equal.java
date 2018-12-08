fde8b6799604d0d14dbdde9351afa40bb933dbcc
mihaiost <45467031+mihaiost@users.noreply.github.com>
2018-12-09 00:54:22 +0200

    Update Whether_2_int_arrays_are_equal.java

--- src/main/java/Whether_2_int_arrays_are_equal.java
+++ src/main/java/Whether_2_int_arrays_are_equal.java
@@ -1,3 +1,12 @@
+/*Write a program that test whether 2 int arrays are equal. Equality in this case means that both arrays have the same number of elements an element on a specific index in first array is equal to the element on the same index in the second array.
+
+For example
+
+For the following arrays [1,2,3,4] and [1,2,3,4] the output should be TRUE
+For the following arrays [] and [] the output should be TRUE
+For the following arrays [1,3,2] and [1,3] the output should be FALSE
+For the following arrays [1,2,3,4] and [1,2,4,3] the output should be FALSE*/
+
 package main.java;
 
 public class Whether_2_int_arrays_are_equal {
