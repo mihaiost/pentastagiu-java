28841033c09d354f7bcc2ccb2ff149788941a9d6
mihaiost <45467031+mihaiost@users.noreply.github.com>
2018-12-09 00:44:04 +0200

    Update Elements_index_in_array.java

--- src/main/java/Elements_index_in_array.java
+++ src/main/java/Elements_index_in_array.java
@@ -1,3 +1,8 @@
+/*Write a java program to return the index of a target element.
+
+For example:
+Given  the following array [13,20,14,5,2,8] and a target element 5 the output should be : 3 (because 5 is located at index 3 in the array)*/
+
 package main.java;
 import java.util.Scanner;
 public class Elements_index_in_array {
